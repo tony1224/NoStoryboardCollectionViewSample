a27553f3348b0e1bd73f4e9b54365cb63252adc6
Morita Jun <moritajun@Morita-no-MacBook-Pro.local>
2017-11-29 15:46:54 +0900

    Initial Commit

---
+++ NoStoryboardCollectionViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoStoryboardCollectionViewSample
+//
+//  Created by Morita Jun on 2017/11/29.
+//  Copyright © 2017年 Morita Jun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
