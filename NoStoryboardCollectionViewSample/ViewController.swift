56f55964cee29186c483560bda3a98edd45cc30d
Morita Jun <moritajun@Morita-no-MacBook-Pro.local>
2017-11-29 16:37:07 +0900

    first commit

--- NoStoryboardCollectionViewSample/ViewController.swift
+++ NoStoryboardCollectionViewSample/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource {
 
+    var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // コレクションビューの設定
+        setupCollectionView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,50 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    private func setupCollectionView() {
+        
+        // 多分これがUICollectionViewDelegateFlowLayoutだと画面サイズに合わせてサイズを動的にーってのと相反する処理
+        
+        // NOTE：全てのCellを同じ大きさで表示する際にこの設定を行う
+        let flowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        // 縦か横か
+        flowLayout.scrollDirection = .vertical
+        // 余白設定の1つ。列間の余白の最小値を指定
+        flowLayout.minimumInteritemSpacing = 5.0
+        // 余白設定の1つ。行間の余白の値を設定
+        flowLayout.minimumLineSpacing = 5.0
+        // NOTE: これをすると動的な変更は行えないので注意
+        flowLayout.itemSize = CGSize(width: 100, height: 100)
+        
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: flowLayout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.dataSource = self
+        self.view.addSubview(collectionView)
+        
+        setupCollectionConstrains()
+    }
+    
+    private func setupCollectionConstrains() {
 
+        // NSLayoutAnchorで
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        collectionView.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        collectionView.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor).isActive = true
+    }
+    
+     // CollectionViewDataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+    
 }
 
